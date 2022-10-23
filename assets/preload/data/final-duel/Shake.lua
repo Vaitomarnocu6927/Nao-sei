2e08516bdd013b7011d148d62d2dc33c34e9db1b
Vaitomarnocu6927 <116380632+Vaitomarnocu6927@users.noreply.github.com>
2022-10-22 23:29:03 -0400

    Add files via upload

---
+++ assets/preload/data/final-duel/Shake.lua
@@ -0,0 +1,21 @@
+function opponentNoteHit(id, direction, noteType, isSustainNote)  
+cameraShake('game', 0.01, 0.05)
+end
+function onCreatePost()
+		    for i = 0,3 do 
+                setPropertyFromGroup('opponentStrums', i, 'x', -5000);
+                setPropertyFromGroup('playerStrums', 0, 'x', defaultPlayerStrumX0 - 320);
+                setPropertyFromGroup('playerStrums', 1, 'x', defaultPlayerStrumX1 - 320);
+                setPropertyFromGroup('playerStrums', 2, 'x', defaultPlayerStrumX2 - 320);
+                setPropertyFromGroup('playerStrums', 3, 'x', defaultPlayerStrumX3 - 320);
+
+	setProperty('scoreTxt.visible', false)
+	setProperty('timeBar.visible', false)
+	setProperty('timeBarBG.visible', false)
+	setProperty('timeTxt.visible', false)
+end
+end
+
+
+
+
